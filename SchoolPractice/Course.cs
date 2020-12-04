917400e832231fa4579b80e385c0990eb3f841a9
Alex Davies <ro.morningstar@gmail.com>
2020-12-04 14:52:49 -0600

    Completed custom methods ToString(), Equals(), and GetHashCode() for Course.cs

--- SchoolPractice/Course.cs
+++ SchoolPractice/Course.cs
@@ -9,12 +9,26 @@ namespace SchoolPractice
         public Teacher Instructor { get; set; }
         public List<Student> enrolledStudents { get; set; }
 
-
         // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather than
         //  just the class fields.
-
+        public override string ToString()
+        {
+            return $"Course Topic: {Topic}; Course Instructor: {Instructor}; Course Roster: {enrolledStudents}";
+        }
 
         // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
         //  Course objects equal.
+        public override bool Equals(object obj)
+        {
+            return obj is Course course &&
+                   Topic == course.Topic &&
+                   EqualityComparer<Teacher>.Default.Equals(Instructor, course.Instructor) &&
+                   EqualityComparer<List<Student>>.Default.Equals(enrolledStudents, course.enrolledStudents);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Topic, Instructor, enrolledStudents);
+        }
     }
 }
