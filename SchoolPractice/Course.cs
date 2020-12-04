3e22219bbfc6e745ebcffc53f580535782c630ca
Alex Davies <ro.morningstar@gmail.com>
2020-12-04 14:15:35 -0600

    Tried adding a fullName method to Teacher class

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+        private List<Student> StudentRoster { get; set; }
+        private int ClassPeriod { get; set; }
+        private string CourseSubject { get; set; }
+        private string TeacherName { get; set; }
+
+        public Course(List<Student> studentRoster, int classPeriod, string courseSubject, string teacherName)
+        {
+            StudentRoster = studentRoster;
+            ClassPeriod = classPeriod;
+            CourseSubject = courseSubject;
+            TeacherName = teacherName;
+        }
+    }
+}
---
+++ SchoolPractice/Teacher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Teacher
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Subject { get; set; }
+        private int YearsTeaching { get; set; }
+
+        public Teacher(string firstName, string lastName, string subject, int yearsTeaching)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Subject = subject;
+            YearsTeaching = yearsTeaching;
+        }
+
+        public string teacherFullName ()
+        {
+            string fullName = $"{LastName}, {FirstName}";
+            return fullName;
+        }
+
+    }
+}
